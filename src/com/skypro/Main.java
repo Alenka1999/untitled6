de83222a4580ccafbe8761f31a3c804fd47a7039
Alenka99 <alena.sosnova@list.ru>
2022-03-15 20:47:34 +0300

    Коммит1

---
+++ src/com/skypro/Main.java
@@ -0,0 +1,27 @@
+package com.skypro;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Задание 1
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i); i ++;
+        }
+        int i1= 10;
+        for (i1 = 10; i1 >=1; i1--) {
+            System.out.println(i1);
+        }
+
+        // Задание 2
+
+        int k = 5;
+        for (k = 5; k <=31; k = k +7) {
+            System.out.println("Сегодня пятница" + k + "Необходимо подготовить отчет");
+        }
+
+        // Задание 3
+
+
+    }
+}
